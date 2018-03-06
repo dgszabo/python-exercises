a95a0481b4cc59b75a8d540a4307867737bbb324
Daniel Szabo <dgszabo@gmail.com>
2018-03-06 15:51:16 -0800

    Render basic template with basic variables

--- flask-routing-templating/app.py
+++ flask-routing-templating/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, template_rendered, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
 @app.route('/person/<name>/<age>')
 def name_and_age(name, age):
-    return render_template('name_and_age.html')
+    return render_template('name_and_age.html', name = name, age = age)
 
 if __name__ == '__main__':
     app.run(debug = True, port = 3000)

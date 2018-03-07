e5820a2f4256c7cc49bd8b237676de1151615a55
Daniel Szabo <dgszabo@gmail.com>
2018-03-07 11:38:51 -0800

    Car app - basic files created

---
+++ cars-app/car.py
@@ -0,0 +1,10 @@
+class Car():
+    
+    id = 1
+    
+    def __init__(self, year, make, model):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.id = Car.id
+        Car.id += 1

a94896f47ce9af01b50fd21c4ce7c464f63c83ce
Daniel Szabo <dgszabo@gmail.com>
2018-03-06 16:32:23 -0800

    Calculator backend functionality completed

--- flask-routing-templating/app.py
+++ flask-routing-templating/app.py
@@ -6,5 +6,23 @@ app = Flask(__name__)
 def name_and_age(name, age):
     return render_template('name_and_age.html', name = name, age = age)
 
+@app.route('/calculator')
+def calculator():
+    return render_template('calculator.html') 
+
+@app.route('/math')
+def math():
+    n1 = float(request.args.get('first_number'))
+    n2 = float(request.args.get('second_number'))
+    operation = request.args.get('operation')
+    if(operation == 'add'):
+        return str(n1 + n2)
+    elif(operation == 'subtract'):
+        return str(n1 - n2)
+    if(operation == 'multiply'):
+        return str(n1 * n2)
+    if(operation == 'divide'):
+        return str(n1 / n2)
+
 if __name__ == '__main__':
     app.run(debug = True, port = 3000)

bdd57bf141871b33060ad486ebdf26cc2c946bc8
Daniel Szabo <dgszabo@gmail.com>
2018-03-11 15:26:41 -0700

    Bootcamp-app - main page map added and functionality implemented

--- bootcamps-app/app.py
+++ bootcamps-app/app.py
@@ -34,7 +34,18 @@ def index():
         db.session.add(new_bootcamp)
         db.session.commit()
         return redirect(url_for('index'))
-    return render_template('index.html', bootcamps = Bootcamp.query.order_by(Bootcamp.votes.desc()))
+    bootcamps = Bootcamp.query.order_by(Bootcamp.votes.desc())
+    src = 'https://maps.googleapis.com/maps/api/staticmap?size=640x640&scale=2&maptype=roadmap\&'
+    for el in bootcamps:
+        location_url = el.location.split(' ')[0]
+        for elem in el.location.split(' ')[1:]:
+            if location_url[-1] == ',':
+                location_url += elem
+            else:
+                location_url += '+' + elem
+        src += 'markers=size:normal%7Ccolor:red%7Clabel:' + el.name[0] + '%7C' + location_url + '&'
+    src += 'key=AIzaSyBWMPmIKv8AHzMHfSFzOSAVoPo2kykxRws'
+    return render_template('index.html', bootcamps = bootcamps, src = src)
 
 @app.route('/bootcamps/new')
 def new():

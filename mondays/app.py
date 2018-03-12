a198c1c09d501d2d9d6c0a4ac3dd14dbf1b6fadd
Daniel Szabo <dgszabo@gmail.com>
2018-03-12 10:44:48 -0700

    Mondays app - migration func added

--- mondays/app.py
+++ mondays/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/mondays'
 app.config['SQLALCHEMY_ECHO'] = True
 
 # this is practically connecting us to the ORM
-db = SQLAlchemy(app) 
+db = SQLAlchemy(app)
+Migrate(app, db)
 
 class Day(db.Model):
 
@@ -14,8 +16,10 @@ class Day(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.Text)
+    mood = db.Column(db.Text)
 
-    def __init__(self, name):
+    def __init__(self, name, mood):
         self.name = name
+        self.mood = mood
 
 
